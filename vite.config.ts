ca72caca191d54d6151e9f2923ff89c1b9b6aa89
MsPritee <badgujarpriti5@gmail.com>
2026-08-30 00:24:59 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    cssMinify: 'esbuild',
-  },
   test: {
     environment: 'jsdom',
     globals: true,
